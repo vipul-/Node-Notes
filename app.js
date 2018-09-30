5925ff40a804aaebd0aa9405c0c04d5fc8ed1bac
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-09-30 22:33:25 +0545

    Cleanup

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 console.log("App Started");
 
 const fs = require('fs');
-const os = require('os');
+const _ = require("lodash");
+
 const notes = require('./notes');
-const _ = require('lodash');
-
-var arr = [1,2,1, "Andrew", "Azure", "Andrew"];
-
-var u = _.uniq(arr);
-console.log(u);
-
-newNote = notes.addNote();
-
-console.log(newNote);
-var user = os.userInfo();
-
-add = notes.add(6, 12);
-
-console.log(add);
-
-// fs.appendFile("greetings.txt", `Hello ${user.username}!`, (err) => {
-//     if(err) {
-//         console.log("There was an error :(");
-//         console.error(err);
-//     }
-// });
--- notes.js
+++ notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
     console.log("New Note");
     return "Note Added";
 };
-
-module.exports.add = (x, y) => {
-    return x+y;
-}
