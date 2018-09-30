d32fa8cf9590f3b512c34e2b6e43bdcba0579623
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-09-30 22:52:51 +0545

    Getting input from user

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require("lodash");
 
 const notes = require('./notes');
+
+var command = process.argv[2];
+
+console.log(`command: ${command}`);
+
+if (command === 'add') {
+    console.log("Adding new notes.");
+} else if (command === 'list') {
+    console.log("Listing notes");
+} else if (command === 'remove') {
+    console.log("Removing notes");
+} else if (command === 'read') {
+    console.log("Fetching notes");
+} else {
+    console.log("Command not recognized");
+}
