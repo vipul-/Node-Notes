fad19ad0fb689682b4fd357274de92af3cf6540f
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-10-11 16:00:39 +0545

    Adding and Saving Notes

--- notes.js
+++ notes.js
@@ -1,7 +1,29 @@
 console.log("required successfully!");
 
+const fs = require("fs");
+
 var addNote = (title, body) => {
-    console.log(`Adding Note \n ${title} \n ${body}`);
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync("notes-data.json");
+        var notes = JSON.parse(notesString);
+    } catch(e) {
+        var notes = [];
+    }
+    
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+        console.log(`Adding Note \n ${title} \n ${body}`);
+    } else {
+        console.log("Duplicate Note");
+    }
 };
 
 var getAll = () => {
